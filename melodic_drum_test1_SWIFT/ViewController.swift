b9359d838492eb3449eb19e1e18fa75a7554260c
Vijay Weemhoff <vijay.w@hotmail.com>
2016-05-10 21:39:55 +0200

    Initial Commit

---
+++ melodic_drum_test1_SWIFT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  melodic_drum_test1_SWIFT
+//
+//  Created by Vijay Weemhoff on 10-05-16.
+//  Copyright © 2016 Vijay Weemhoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ melodic_drum_test1_SWIFTTests/melodic_drum_test1_SWIFTTests.swift
@@ -0,0 +1,36 @@
+//
+//  melodic_drum_test1_SWIFTTests.swift
+//  melodic_drum_test1_SWIFTTests
+//
+//  Created by Vijay Weemhoff on 10-05-16.
+//  Copyright © 2016 Vijay Weemhoff. All rights reserved.
+//
+
+import XCTest
+@testable import melodic_drum_test1_SWIFT
+
+class melodic_drum_test1_SWIFTTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
