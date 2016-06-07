7ed477e057f504a67fe5359a73d2b84f16171070
Vijay Weemhoff <vijay.w@hotmail.com>
2016-06-07 20:51:24 +0200

    added delete function in load preset

--- melodic_drum_test1_SWIFT/presetViewController.swift
+++ melodic_drum_test1_SWIFT/presetViewController.swift
@@ -8,8 +8,21 @@
 
 import UIKit
 
+func deleteFromPresetArray(){
+    //delete from array
+    presetArray.removeObjectAtIndex(indexDelete)
+    
+    //save the presetArray in NSDefault so it can be default.
+    NSUserDefaults.standardUserDefaults().setObject(presetArray, forKey: "presets")
+    NSUserDefaults.standardUserDefaults().synchronize()
+}
+
+var indexDelete = 0
+
 class presetViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var items = presetArray
+    var copyPresetArray: NSMutableArray = [""]
+    
     
     @IBOutlet var tableView: UITableView?
     
@@ -25,6 +38,31 @@ class presetViewController: UIViewController, UITableViewDelegate, UITableViewDa
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func loadAndGoBackPressed(sender: UIButton) {
+        mutArray = copyPresetArray
+    }
+    
+    @IBAction func AlertSure(){
+        let alertController = UIAlertController(title: "Warning", message: "Are you sure you want to delete this preset?", preferredStyle:UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: nil))
+        alertController.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.Default){
+            action -> Void in
+            //NSLog("Pressed Yes")
+            //delete from tableView
+            //self.items.removeObjectAtIndex(indexDelete)
+            
+            //send data to other viewController
+            deleteFromPresetArray()
+            self.tableView!.reloadData()
+        
+            //NSUserDefaults.standarUserDefaults().removeObjectForKey(HIERWELKEJEWILTVERWIJDEREN)
+            })
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func deletePresetPressed(sender: UIButton) {
+        AlertSure()
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -42,8 +80,13 @@ class presetViewController: UIViewController, UITableViewDelegate, UITableViewDa
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         //NSLog("You selected cell #\(indexPath.row)!")
         NSLog("\(presetArray[indexPath.row])")
+        //if you want to delete you need index
+        indexDelete = indexPath.row
+        NSLog("%d",indexDelete)
+        
+        //first copy the the preset array
         if let tempNames: NSArray = NSUserDefaults.standardUserDefaults().arrayForKey(presetArray[indexPath.row] as! String) {
-            mutArray = tempNames.mutableCopy() as! NSMutableArray
+            copyPresetArray = tempNames.mutableCopy() as! NSMutableArray
         }
     }
     
